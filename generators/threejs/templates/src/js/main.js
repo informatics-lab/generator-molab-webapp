84e59e6e1aa5cf0fa2a9fddc162513268df6aeca
Thomas Powell <t.powell.meto@gmail.com>
2016-11-23 17:06:05 +0000

    add new gulp functionality

--- generators/threejs/templates/src/js/main.js
+++ generators/threejs/templates/src/js/main.js
@@ -1,63 +1,61 @@
+/* eslint-env browser */
 'use strict';
 
-var Stats = require('stats-js');
-var THREE = require('three');
+import THREE from "three";
 
-var stats;
-var scene, camera, renderer;
+let scene;
+let camera;
+let renderer;
 
+/**
+ * Initialises everything
+ */
 function init() {
+  let width = window.innerWidth;
+  let height = window.innerHeight;
 
-  var width = window.innerWidth;
-  var height = window.innerHeight;
-
-  //create threejs redenderer and attach it to dom
+  // create threejs redenderer and attach it to dom
   renderer = new THREE.WebGLRenderer({antialias: true});
   renderer.setSize(width, height);
-  renderer.setClearColor("grey");
+  renderer.setClearColor('grey');
   document.body.appendChild(renderer.domElement);
 
-  //create threejs scene
+  // create threejs scene
   scene = new THREE.Scene();
 
-  //create our camera
+  // create our camera
   camera = new THREE.PerspectiveCamera(45, width / height, 0.01, 1000);
   camera.position.z = 5;
 
-  //resize threejs canvas if window size is changed.
+  /**
+   * Updates threejs camera & renderer in accordance with changes to
+   * the browser window
+   */
   function onWindowResize() {
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
     renderer.setSize(window.innerWidth, window.innerHeight);
-  }
+  };
+
   window.addEventListener('resize', onWindowResize, false);
 
-  stats = new Stats();
-  stats.domElement.style.position = 'absolute';
-  stats.domElement.style.left = '0px';
-  stats.domElement.style.bottom = '0px';
-  document.body.appendChild( stats.domElement );
+  // add stuff to our scene ...
+  let geometry = new THREE.CubeGeometry(size, size);
+  let material = new THREE.MeshBasicMaterial({color: 'blue'});
+  let mesh = new THREE.Mesh(geometry, material);
 
-  //add stuff to our scene here...
-  var geometry = new THREE.CubeGeometry(size,size);
-  var material = new THREE.MeshBasicMaterial({
-    color:"blue"
-  });
-  var mesh = new THREE.Mesh(geometry, material);
+  self.scene.add(mesh);
 
-  scene.add(mesh);
-
-
-
-
-  animate();
+  animate(0);
 }
 
-// animation loop
+/**
+ * Animation loop
+ * @param {number} time time since function was last called
+ */
 function animate(time) {
-  scene.dispatchEvent({type: "animate", message: time});
-  camera.dispatchEvent({type:"animate", message: time});
-  stats.update();
+  scene.dispatchEvent({type: 'animate', message: time});
+  camera.dispatchEvent({type: 'animate', message: time});
   requestAnimationFrame(animate);
   renderer.render(scene, camera);
 }
